44916a9fb375cb748e035c0265fb91ec6d191d89
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2018-07-03 09:39:58 +0000

    Removed test code
    git-svn-id: https://server.mycore.de/svn/ubo/trunk@37750 7089d063-bc14-496d-ac90-53de1872eb6c

--- src/main/java/unidue/ubo/importer/JSON2XMLTransformer.java
+++ src/main/java/unidue/ubo/importer/JSON2XMLTransformer.java
@@ -1,15 +1,12 @@
 package unidue.ubo.importer;
 
 import java.io.IOException;
-import java.net.URL;
 
 import org.mycore.common.content.MCRContent;
 import org.mycore.common.content.MCRStringContent;
-import org.mycore.common.content.MCRURLContent;
 import org.mycore.common.content.transformer.MCRContentTransformer;
 import org.mycore.common.content.transformer.MCRContentTransformerFactory;
 import org.mycore.common.content.transformer.MCRParameterizedTransformer;
-import org.mycore.common.events.MCRStartupHandler;
 import org.mycore.common.xsl.MCRParameterCollector;
 
 /**
@@ -20,19 +17,6 @@ import org.mycore.common.xsl.MCRParameterCollector;
  */
 public class JSON2XMLTransformer extends MCRContentTransformer {
 
-    public static void main(String[] args) throws Exception {
-        MCRStartupHandler.startUp(null);
-
-        String url = "https://api.oadoi.org/v2/10.1038/nature12373?email=frank.luetzenkirchen@uni-due.de";
-        MCRContent json = new MCRURLContent(new URL(url));
-        MCRContent copy = json.getReusableCopy();
-        System.out.println(copy.asString());
-
-        MCRContentTransformer json2xml = MCRContentTransformerFactory.getTransformer("json2xml");
-        MCRContent out = json2xml.transform(json);
-        System.out.println(out.asString());
-    }
-
     @Override
     public MCRContent transform(MCRContent json) throws IOException {
         MCRContent dummy = new MCRStringContent("<dummy/>");
@@ -42,6 +26,5 @@ public class JSON2XMLTransformer extends MCRContentTransformer {
         params.setParameter("json", json.asString());
         MCRContent result = pt.transform(dummy, params);
         return result;
-
     }
 }
