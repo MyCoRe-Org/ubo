41f2a20c16548c1619c35ba2374a2bcff2b819e6
Pascal <rost@informatik.uni-hamburg.de>
2019-12-12 13:19:38 +0100

    Fixed autocomplete not using jsonp for remote solr query

--- src/main/webapp/js/project_autocomplete.js
+++ src/main/webapp/js/project_autocomplete.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
 
     // config variables
-    let target_solr_core = 'ubo_projects';
+    let solr_url = 'solr/ubo_projects/select/';
     let id_project_acronym = 'project_acronym';
     let id_project_title = 'project_title';
     let min_length = 0;
     let input_delay = 200;
+    let max_label_length = 70;
 
     // derived settings
     let acronym_input = $('#' + id_project_acronym);
@@ -14,15 +15,24 @@ $(document).ready(function(){
     let call_solr = function(request, response) {
     	let search_string = request.term.toLowerCase();
         let solr_search_string = 'project_search_all:' + search_string + '*';
-        let solr_call = 'solr/' + target_solr_core + '/select/?q=' + solr_search_string + '&wt=json&fl=*';
-        $.getJSON(solr_call, function(data) {
+        $.ajax({
+           url: solr_url,
+           dataType: 'jsonp',
+           jsonp: 'json.wrf',
+           data: {
+               q: solr_search_string,
+               wt: 'json',
+               fl: '*'
+           },
+           success: function(data) {
             let docs = data.response.docs;
             let sources = [];
             $.each(docs, function(i, doc) {
-                let source_label = doc.acronym + ': ' + doc.projekttitel;
+                let source_label = (doc.acronym + ': ' + doc.projekttitel).substring(0, max_label_length) + '...';
                 sources.push({'label': source_label, 'projekttitel': doc.projekttitel, 'acronym': doc.acronym});
             });
             response(sources);
+        }
         });
     }
 
