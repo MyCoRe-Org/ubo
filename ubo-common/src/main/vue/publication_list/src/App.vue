19bb7863778c115a387cfe3b7179795724cbf43a
Kathleen Neumann <neumann@gbv.de>
2021-05-07 21:30:33 +0200

    bring layout in sync with current ubo forms

--- ubo-common/src/main/vue/publication_list/src/App.vue
+++ ubo-common/src/main/vue/publication_list/src/App.vue
@@ -2,35 +2,33 @@
   <div class="ubo-vue-publication-list ">
     <link v-if="bootstrap" rel="stylesheet" :href="bootstrap">
     <link v-if="css" rel="stylesheet" :href="css">
-    <section class="row">
-      <div class="col-12">
-        <label for="personSearch">{{i18n["index.person"]}}</label>
-        <div class="input-group">
-          <input id="personSearch" class="form-control" type="text" v-model="search.text"
-                 v-on:keyup.enter="startSearch">
-          <span class="input-group-btn">
-            <button class="btn btn-secondary" v-on:click="startSearch">
-              <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"
-                    v-if="search.searching"></span>
-              {{ i18n["button.search"] }}
-            </button>
-          </span>
-        </div>
+    <section>
+      <div class="form-group form-inline">
+        <label class="mycore-form-label" for="personSearch">{{i18n["index.person"]}}</label>
+        <input id="personSearch" class="mycore-form-input" type="text" v-model="search.text"
+               v-on:keyup.enter="startSearch">
+        <span class="input-group-btn">
+          <button class="btn btn-secondary" v-on:click="startSearch">
+            <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"
+                  v-if="search.searching"></span>
+            {{ i18n["button.search"] }}
+          </button>
+        </span>
       </div>
     </section>
-    <section class="row mt-2" v-if="search.errored">
-      <div class="col-12">
-        <div class="p-3 mb-2 bg-danger text-white">
-          {{ i18n["error.occurred"] }}
-        </div>
+    <section v-if="search.errored">
+      <div class="ubo-input-error">
+        {{ i18n["error.occurred"] }}
       </div>
     </section>
-    <section class="row mt-2" v-if="search.searchResultUsers.length>0 || search.noresults">
-      <div class="col-12">
+    <section v-if="search.searchResultUsers.length>0 || search.noresults">
+      <div class="ubo-pl-noresults">
         <b>{{ i18n["result.dozbib.results"] }}</b>
-        <div v-if="search.noresults" v-on:click="resetSearch()" class="p-3 mb-2 bg-info text-white">
+        <div v-if="search.noresults" v-on:click="resetSearch()">
           <a href="javascript:void(0)">{{ i18n["index.person.found.0"] }}</a>
         </div>
+      </div>
+      <div class="ubo-pl-results">
         <div class="list-group list-group-flush">
           <a class="list-group-item list-group-item-action"
              v-for="autocompleteUser in search.searchResultUsers"
@@ -42,8 +40,8 @@
         </div>
       </div>
     </section>
-    <section class="row mt-2" v-if="users.length>0">
-      <div class="col-12">
+    <section v-if="users.length>0">
+      <div class="ubo-pl-userlist">
         <b>{{ i18n["listWizard.users"] }}</b>
         <div class="list-group list-group-flush">
           <a class="list-group-item list-group-item-action"
@@ -56,33 +54,33 @@
         </div>
       </div>
     </section>
-    <section class="row mt-2">
-      <div class="col-12">
-        <label for="yearIssued">{{i18n["search.dozbib.year.publication"]}}</label>
-        <input id="yearIssued" v-model="exportM.year" class="form-control"
+    <section>
+      <div class="form-group form-inline">
+        <label class="mycore-form-label" for="yearIssued">{{i18n["search.dozbib.year.publication"]}}</label>
+        <input id="yearIssued" v-model="exportM.year" class="mycore-form-input"
                v-bind:class="{ 'is-invalid' : isInvalidYear() }" v-on:change="yearChange"
                type="number">
-        <div class="invalid-feedback">
+        <div class="ubo-input-invalid invalid-feedback">
           {{i18n["search.dozbib.year.invalid"]}}
         </div>
       </div>
     </section>
-    <section class="row mt-2">
-      <div class="col-12">
-        <label for="sortSelect">{{i18n["search.sort"]}}</label>
-        <select id="sortSelect" class="form-control custom-select" v-on:change="sortChange" v-model="exportM.sortField">
+    <section>
+      <div class="form-group">
+        <label class="mycore-form-label" for="sortSelect">{{i18n["search.sort"]}}</label>
+        <select id="sortSelect" class="mycore-form-input custom-select" v-on:change="sortChange" v-model="exportM.sortField">
           <option v-bind:value="'year'">{{i18n["search.sort.year"]}}</option>
           <option v-bind:value="'sortby_person'">{{ i18n["search.sort.name"] }}</option>
           <option v-bind:value="'sortby_title'">{{ i18n["search.sort.title"] }}</option>
         </select>
-        <div class="form-check">
+        <div class="ubo-input-check form-check">
           <input class="form-check-input" v-on:change="sortChange" type="checkbox" id="ascSort" v-model="exportM.asc">
           <label class="form-check-label" for="ascSort">{{i18n["search.sort.asc"]}}</label>
         </div>
       </div>
-      <div class="col-12 mt-2">
-        <label for="formatSelect">{{ i18n["listWizard.format"] }}</label>
-        <select id="formatSelect" class="form-control custom-select" v-on:change="formatChange" v-model="exportM.format">
+      <div class="form-group form-inline">
+        <label class="mycore-form-label" for="formatSelect">{{ i18n["listWizard.format"] }}</label>
+        <select id="formatSelect" class="mycore-form-input custom-select" v-on:change="formatChange" v-model="exportM.format">
           <option v-bind:value="''">{{ i18n["search.select"] }}</option>
           <option v-bind:value="'pdf'">PDF</option>
           <option v-bind:value="'html'">HTML</option>
@@ -94,24 +92,26 @@
           <option v-bind:value="'mods2csv'">CSV</option>
         </select>
       </div>
-      <div class="col-12" v-if="exportM.format==='html' || exportM.format==='pdf'">
-        <label for="styleSelect">{{ i18n["listWizard.citation"] }}</label>
-        <select id="styleSelect" class="form-control custom-select" v-on:change="styleChange" v-model="exportM.style">
+      <div class="form-group form-inline" v-if="exportM.format==='html' || exportM.format==='pdf'">
+        <label class="mycore-form-label" for="styleSelect">{{ i18n["listWizard.citation"] }}</label>
+        <select id="styleSelect" class="mycore-form-input custom-select" v-on:change="styleChange" v-model="exportM.style">
           <option v-bind:value="''">{{ i18n["search.select"] }}</option>
           <option v-for="style in styles" v-bind:key="style.id" v-bind:value="style.id">{{style.title}}</option>
         </select>
       </div>
     </section>
-    <section class="row mt-2" v-if="result.link.length>0">
-      <div class="col-12">
-        <!-- when the format is html, we will show the link (which is a iframe) as code -->
-        <p v-if="exportM.format==='html'">{{ i18n["listWizard.code"] }}</p>
-        <code v-if="exportM.format==='html'">&lt;iframe scrolling=&quot;yes&quot; width=&quot;90%&quot; height=&quot;300&quot;
-          src=&quot;{{result.link}}&quot;/&gt;</code>
+    <section v-if="result.link.length>0">
+      <div class="row">
+        <div class="col-12">
+          <!-- when the format is html, we will show the link (which is a iframe) as code -->
+          <p v-if="exportM.format==='html'">{{ i18n["listWizard.code"] }}</p>
+          <code v-if="exportM.format==='html'">&lt;iframe scrolling=&quot;yes&quot; width=&quot;90%&quot; height=&quot;300&quot;
+            src=&quot;{{result.link}}&quot;/&gt;</code>
 
-        <!-- when format is not html, we just show it as link -->
-        <p v-if="exportM.format!=='html'">{{ i18n["listWizard.link"] }}</p>
-        <a v-if="exportM.format!=='html'" :href="result.link">{{result.link}}</a>
+          <!-- when format is not html, we just show it as link -->
+          <p v-if="exportM.format!=='html'">{{ i18n["listWizard.link"] }}</p>
+          <a v-if="exportM.format!=='html'" :href="result.link">{{result.link}}</a>
+        </div>
       </div>
     </section>
   </div>
