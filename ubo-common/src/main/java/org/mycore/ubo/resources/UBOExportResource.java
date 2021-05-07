cdb05f3ddc64151c703a36e7f6eb931bf466d845
Sebastian Hofmann <7668803+sebhofmann@users.noreply.github.com>
2021-05-07 21:34:33 +0200

    UBO-72 refacture publication list use mycore csl here
    * Added year and some improvements
    * Use custom-select
    * Rename resource and wrap the request to solr
    * Use comma instead of semicolon as seperator for pids
    * Set field type of name to ubo_string_ci instead of string
    * Add class for root element. Add ie warning.
    * bring layout in sync with current ubo forms
    
    Co-authored-by: Kathleen Neumann <neumann@gbv.de>

---
+++ ubo-common/src/main/java/org/mycore/ubo/resources/UBOExportResource.java
@@ -0,0 +1,139 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mycore.ubo.resources;
+
+import com.google.gson.Gson;
+import org.jdom2.Element;
+import org.jdom2.Namespace;
+import org.mycore.common.config.MCRConfiguration2;
+import org.mycore.common.xml.MCRURIResolver;
+import org.mycore.frontend.MCRFrontendUtil;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.PathSegment;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+@Path("export")
+public class UBOExportResource {
+
+    public static final Namespace CSL_NAMESPACE = Namespace.getNamespace("csl", "http://purl.org/net/xbiblio/csl");
+
+    private static String encode(String s) {
+        return URLEncoder.encode(s, StandardCharsets.UTF_8);
+    }
+
+    @GET
+    @Path("styles")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listStyles(){
+        String[] styles = MCRConfiguration2.getStringOrThrow("MCR.Export.CSL.Styles").split(",");
+
+        List<CSLEntry> result = new ArrayList<>(styles.length);
+        for (String style : styles) {
+            Element element = MCRURIResolver.instance().resolve("resource:" + style + ".csl");
+
+            String title = Optional.ofNullable(element.getChild("info", CSL_NAMESPACE))
+                    .map(el -> Optional.ofNullable(el.getChild("title", CSL_NAMESPACE)))
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .map(Element::getTextNormalize).orElse(style);
+
+            CSLEntry cslEntry = new CSLEntry(style, title);
+            result.add(cslEntry);
+        }
+
+        String json = new Gson().toJson(result.toArray(), CSLEntry[].class);
+        return Response.ok(json).build();
+    }
+
+    @GET
+    @Path("link/{format}/{pids:.+}")
+    public Response link(
+        @PathParam("format") String format,
+        @PathParam("pids") String pidSegment,
+        @QueryParam("sortField") String sortField,
+        @QueryParam("sortDirection") String sortDirection,
+        @QueryParam("year") int year,
+        @QueryParam("style") String style) throws URISyntaxException {
+
+        Set<String> pids = Stream.of(pidSegment.split(",")).collect(Collectors.toSet());
+
+        if (pids.size() == 0) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+
+        if(sortField == null) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+
+        if(sortDirection == null) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+
+        if(year == -1) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+
+        String nidConnectionValue = "(" + String.join(" OR ", pids) + ")";
+        String baseURL = MCRFrontendUtil.getBaseURL();
+
+        String solrQuery = "nid_connection:" + nidConnectionValue + " and year:>=" + year;
+        StringBuilder solrRequest = new StringBuilder()
+            .append(baseURL).append("servlets/solr/select")
+            .append("?q=").append(encode(solrQuery))
+            .append("&rows=9999&")
+            .append("&sort=").append(encode(sortField)).append(encode(" ")).append(encode(sortDirection));
+
+        if (style == null) {
+            solrRequest.append("&XSL.Transformer=").append(encode(format));
+        } else {
+            solrRequest.append("&XSL.Transformer=response-csl-").append(encode(format));
+            solrRequest.append("&XSL.style=").append(encode(style));
+        }
+
+        URI newLocation = new URI(solrRequest.toString());
+        return Response.temporaryRedirect(newLocation).build();
+    }
+
+    public static class CSLEntry {
+        public CSLEntry(String id, String title) {
+            this.id = id;
+            this.title = title;
+        }
+
+        public String id;
+        public String title;
+    }
+}
---
+++ ubo-common/src/main/vue/publication_list/src/App.vue
@@ -0,0 +1,372 @@
+<template>
+  <div class="ubo-vue-publication-list ">
+    <link v-if="bootstrap" rel="stylesheet" :href="bootstrap">
+    <link v-if="css" rel="stylesheet" :href="css">
+    <section>
+      <div class="form-group form-inline">
+        <label class="mycore-form-label" for="personSearch">{{i18n["index.person"]}}</label>
+        <input id="personSearch" class="mycore-form-input" type="text" v-model="search.text"
+               v-on:keyup.enter="startSearch">
+        <span class="input-group-btn">
+          <button class="btn btn-secondary" v-on:click="startSearch">
+            <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"
+                  v-if="search.searching"></span>
+            {{ i18n["button.search"] }}
+          </button>
+        </span>
+      </div>
+    </section>
+    <section v-if="search.errored">
+      <div class="ubo-input-error">
+        {{ i18n["error.occurred"] }}
+      </div>
+    </section>
+    <section v-if="search.searchResultUsers.length>0 || search.noresults">
+      <div class="ubo-pl-noresults">
+        <b>{{ i18n["result.dozbib.results"] }}</b>
+        <div v-if="search.noresults" v-on:click="resetSearch()">
+          <a href="javascript:void(0)">{{ i18n["index.person.found.0"] }}</a>
+        </div>
+      </div>
+      <div class="ubo-pl-results">
+        <div class="list-group list-group-flush">
+          <a class="list-group-item list-group-item-action"
+             v-for="autocompleteUser in search.searchResultUsers"
+             :key="autocompleteUser.pid"
+             v-on:click="addUser(autocompleteUser)"
+             href="javascript:void(0)">
+            {{ autocompleteUser.name }}
+          </a>
+        </div>
+      </div>
+    </section>
+    <section v-if="users.length>0">
+      <div class="ubo-pl-userlist">
+        <b>{{ i18n["listWizard.users"] }}</b>
+        <div class="list-group list-group-flush">
+          <a class="list-group-item list-group-item-action"
+             v-for="user in users"
+             :key="user.pid"
+             v-on:click="removeUser(user)"
+             href="javascript:void(0)">
+            {{ user.name }}
+          </a>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="form-group form-inline">
+        <label class="mycore-form-label" for="yearIssued">{{i18n["search.dozbib.year.publication"]}}</label>
+        <input id="yearIssued" v-model="exportM.year" class="mycore-form-input"
+               v-bind:class="{ 'is-invalid' : isInvalidYear() }" v-on:change="yearChange"
+               type="number">
+        <div class="ubo-input-invalid invalid-feedback">
+          {{i18n["search.dozbib.year.invalid"]}}
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="form-group">
+        <label class="mycore-form-label" for="sortSelect">{{i18n["search.sort"]}}</label>
+        <select id="sortSelect" class="mycore-form-input custom-select" v-on:change="sortChange" v-model="exportM.sortField">
+          <option v-bind:value="'year'">{{i18n["search.sort.year"]}}</option>
+          <option v-bind:value="'sortby_person'">{{ i18n["search.sort.name"] }}</option>
+          <option v-bind:value="'sortby_title'">{{ i18n["search.sort.title"] }}</option>
+        </select>
+        <div class="ubo-input-check form-check">
+          <input class="form-check-input" v-on:change="sortChange" type="checkbox" id="ascSort" v-model="exportM.asc">
+          <label class="form-check-label" for="ascSort">{{i18n["search.sort.asc"]}}</label>
+        </div>
+      </div>
+      <div class="form-group form-inline">
+        <label class="mycore-form-label" for="formatSelect">{{ i18n["listWizard.format"] }}</label>
+        <select id="formatSelect" class="mycore-form-input custom-select" v-on:change="formatChange" v-model="exportM.format">
+          <option v-bind:value="''">{{ i18n["search.select"] }}</option>
+          <option v-bind:value="'pdf'">PDF</option>
+          <option v-bind:value="'html'">HTML</option>
+          <option v-bind:value="'mods'">MODS</option>
+          <option v-bind:value="'bibtex'">BibTex</option>
+          <option v-bind:value="'endnote'">Endnote</option>
+          <option v-bind:value="'ris'">RIS</option>
+          <option v-bind:value="'isi'">ISI</option>
+          <option v-bind:value="'mods2csv'">CSV</option>
+        </select>
+      </div>
+      <div class="form-group form-inline" v-if="exportM.format==='html' || exportM.format==='pdf'">
+        <label class="mycore-form-label" for="styleSelect">{{ i18n["listWizard.citation"] }}</label>
+        <select id="styleSelect" class="mycore-form-input custom-select" v-on:change="styleChange" v-model="exportM.style">
+          <option v-bind:value="''">{{ i18n["search.select"] }}</option>
+          <option v-for="style in styles" v-bind:key="style.id" v-bind:value="style.id">{{style.title}}</option>
+        </select>
+      </div>
+    </section>
+    <section v-if="result.link.length>0">
+      <div class="row">
+        <div class="col-12">
+          <!-- when the format is html, we will show the link (which is a iframe) as code -->
+          <p v-if="exportM.format==='html'">{{ i18n["listWizard.code"] }}</p>
+          <code v-if="exportM.format==='html'">&lt;iframe scrolling=&quot;yes&quot; width=&quot;90%&quot; height=&quot;300&quot;
+            src=&quot;{{result.link}}&quot;/&gt;</code>
+
+          <!-- when format is not html, we just show it as link -->
+          <p v-if="exportM.format!=='html'">{{ i18n["listWizard.link"] }}</p>
+          <a v-if="exportM.format!=='html'" :href="result.link">{{result.link}}</a>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue} from 'vue-property-decorator';
+
+
+@Component
+export default class PublicationList extends Vue {
+  /**
+   * The webapplication base url.
+   * @private
+   */
+  @Prop() private baseurl!: string;
+
+  /**
+   * The url to the bootstrap.css
+   * @private
+   */
+  @Prop() private bootstrap!: string;
+
+  /**
+   * The url to a custom.css
+   * @private
+   */
+  @Prop() private css!: string;
+
+  /**
+   * The name of the leadid
+   * @private
+   */
+  @Prop() private leadid!: string;
+
+  private search: SearchModel = {
+    text: "",
+    searchResultUsers: [],
+    searching: false,
+    noresults: false,
+    errored: false
+  };
+
+  private exportM: ExportModel = {
+    format: "",
+    style: "",
+    sortField: "year",
+    asc: true,
+    year: new Date().getFullYear(),
+  };
+
+  private result = {
+    link: ""
+  }
+
+  private styles: StyleDescription[] = []
+
+  private i18n: { [key: string]: string | null; } = {
+    "button.search": null,
+    "error.occurred": null,
+    "index.person.found.0": null,
+    "listWizard.users": null,
+    "search.select": null,
+    "search.sort": null,
+    "search.sort.year": null,
+    "search.sort.name": null,
+    "search.sort.title": null,
+    "search.sort.asc": null,
+    "listWizard.link": null,
+    "listWizard.code": null,
+    "listWizard.format": null,
+    "listWizard.citation": null,
+    "result.dozbib.results": null,
+    "search.dozbib.year.publication": null,
+    "search.dozbib.year.invalid": null,
+    "index.person": null
+  };
+
+  private users: User[] = [];
+
+  created(): void {
+    this.resolveStyles();
+    this.resolveiI18N();
+  }
+
+  private isInvalidYear() {
+    return isNaN(this.exportM.year) || this.exportM.year > new Date().getFullYear() || this.exportM.year < 0;
+  }
+
+  private async resolveStyles() {
+    let response = await fetch(this.getWebApplicationBaseURL() + "rsc/export/styles");
+    let styles = await response.json();
+    for (const style of styles) {
+      this.styles.push(style);
+    }
+  }
+
+  private async resolveiI18N() {
+    Object.keys(this.i18n)
+        .map(k => {
+          let url = this.getWebApplicationBaseURL() + "rsc/locale/translate/" + k;
+          fetch(url).then(result => {
+            result.text().then(translation => {
+              this.i18n[k] = translation;
+            });
+          })
+        });
+  }
+
+  private startSearch(): void {
+    this.resetSearch();
+    let currentPromise: Promise<User[]> = this.searchSolrForPerson(this.search.text);
+    currentPromise.then(userArray => {
+      if (userArray.length == 0) {
+        this.search.noresults = true;
+      }
+      for (const foundUser of userArray) {
+        this.search.searchResultUsers.push(foundUser);
+      }
+    }).catch(() => {
+      this.search.errored = true;
+    });
+  }
+
+  private resetSearch() {
+    this.search.searchResultUsers.length = 0;
+    this.search.noresults = false;
+  }
+
+  private addUser(user: User) {
+    if (this.users.filter(u => u.pid == user.pid).length == 0) {
+      this.users.push(user);
+    }
+    this.resetSearch();
+    this.createLink();
+  }
+
+  private removeUser(user: User): void {
+    let number = this.users.indexOf(user);
+    this.users.splice(number, 1);
+    this.createLink();
+  }
+
+  private formatChange(): void {
+    this.createLink();
+  }
+
+  private sortChange(): void {
+    this.createLink();
+  }
+
+  private yearChange(): void {
+    this.createLink();
+  }
+
+  private createLink() {
+    this.clearLink();
+
+    if (this.isInvalidYear()) {
+      return;
+    }
+
+    if (this.users.length == 0) {
+      return;
+    }
+
+    let exportModel = this.exportM;
+    if (exportModel.format.length === 0) {
+      return;
+    }
+
+    let query = this.users.map(u => `${u.pid}`).join(",");
+    if (exportModel.format == 'pdf' || exportModel.format == 'html') {
+      if (exportModel.style.length == 0) {
+        return;
+      }
+
+      this.result.link =
+          `${this.getWebApplicationBaseURL()}rsc/export/link/${exportModel.format}/${query}?year=${this.exportM.year}&`+
+          `sortField=${exportModel.sortField}&sortDirection=${exportModel.asc ? "asc" : "desc"}&style=${exportModel.style}`;
+      return;
+    } else {
+      this.result.link =
+          `${this.getWebApplicationBaseURL()}rsc/export/link/${exportModel.format}/${query}?year=${this.exportM.year}&`+
+          `sortField=${exportModel.sortField}&sortDirection=${exportModel.asc ? "asc" : "desc"}`;
+    }
+  }
+
+  private styleChange() {
+    this.clearLink();
+    this.createLink();
+    return;
+  }
+
+  private clearLink() {
+    this.result.link = "";
+  }
+
+  async searchSolrForPerson(name: string): Promise<User[]> {
+    this.search.errored = false;
+    this.search.searching = true;
+    let response = await
+        fetch(`${this.getWebApplicationBaseURL()}servlets/solr/select?q=name_id_connection:* AND (name:*${name}* OR name_id_${this.leadid}:*${name}*)&group=true&group.field=name&fl=*&wt=json`);
+    if (response.ok) {
+      let json = await response.json();
+      this.search.searching = false;
+
+      let results = [];
+      for (const {doclist} of json.grouped.name.groups) {
+        let doc = doclist.docs[0];
+        const result = {name: doc.name, pid: doc.name_id_connection[0]};
+        if (result.name && result.pid) {
+          results.push(result);
+        }
+      }
+      return results;
+    }
+    throw new Error("Error while request person from solr: " + response.statusText);
+  }
+
+  private getWebApplicationBaseURL() {
+    return this.baseurl ;
+  }
+
+
+}
+
+export interface SearchModel {
+  text: string,
+  searchResultUsers: User[],
+  searching: boolean,
+  noresults: boolean,
+  errored: boolean
+}
+
+export interface ExportModel {
+  format: string;
+  style: string;
+  sortField: string;
+  asc: boolean;
+  year: number
+}
+
+export interface StyleDescription {
+  id: string,
+  title: string
+}
+
+export interface User {
+  name: string;
+  pid: string;
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
