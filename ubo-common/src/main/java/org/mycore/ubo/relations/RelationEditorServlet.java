d4f71ac53ad7859012a7b02a5ff59a85c09dd30f
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2022-01-30 23:24:07 +0100

    UBO-127 Removed legacy comment

--- ubo-common/src/main/java/org/mycore/ubo/relations/RelationEditorServlet.java
+++ ubo-common/src/main/java/org/mycore/ubo/relations/RelationEditorServlet.java
@@ -1,12 +1,3 @@
-/**
- * Copyright (c) 2016 Duisburg-Essen University Library
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v2.0
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- **/
-
 package org.mycore.ubo.relations;
 
 import java.util.List;
