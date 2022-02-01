901ecede58d8b7d591d457e0bc4ff32401e34a11
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2022-02-01 21:30:40 +0100

    UBO-127 Refactoring, added merge function, added search button

--- ubo-common/src/main/java/org/mycore/ubo/relations/RelationEditorServlet.java
+++ ubo-common/src/main/java/org/mycore/ubo/relations/RelationEditorServlet.java
@@ -1,5 +1,6 @@
 package org.mycore.ubo.relations;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -7,9 +8,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jdom2.Content;
 import org.jdom2.Element;
+import org.mycore.access.MCRAccessException;
 import org.mycore.common.MCRConstants;
+import org.mycore.datamodel.common.MCRActiveLinkException;
 import org.mycore.datamodel.metadata.MCRMetaLinkID;
 import org.mycore.datamodel.metadata.MCRMetadataManager;
 import org.mycore.datamodel.metadata.MCRObject;
@@ -17,6 +19,7 @@ import org.mycore.datamodel.metadata.MCRObjectID;
 import org.mycore.frontend.servlets.MCRServlet;
 import org.mycore.frontend.servlets.MCRServletJob;
 import org.mycore.mods.MCRMODSWrapper;
+import org.mycore.mods.merger.MCRMergeTool;
 import org.mycore.ubo.AccessControl;
 import org.mycore.ubo.DozBibEntryServlet;
 
@@ -33,106 +36,156 @@ public class RelationEditorServlet extends MCRServlet {
 
         if (AccessControl.systemInReadOnlyMode()) {
             DozBibEntryServlet.sendReadOnlyError(res);
-            return;
-        }
-
-        if (!AccessControl.currentUserIsAdmin()) {
+        } else if (!AccessControl.currentUserIsAdmin()) {
             res.sendError(HttpServletResponse.SC_FORBIDDEN);
-            return;
+        } else {
+            String action = req.getParameter("action");
+
+            if ("delete".equals(action)) {
+                deleteEntry(req, res);
+            } else if ("extractHost".equals(action)) {
+                extractHost(req, res);
+            } else if ("linkHost".equals(action)) {
+                linkHost(req, res);
+            } else if ("unlinkHost".equals(action)) {
+                unlinkHost(req, res);
+            } else if ("merge".equals(action)) {
+                merge(req, res);
+            }
+
+            if (!res.isCommitted()) {
+                String base = req.getParameter("base");
+                res.sendRedirect( "DozBibEntryServlet?id=" + base + "&XSL.Style=structure");
+            }
+        }
+    }
+
+    private MCRObjectID getAndCheck(HttpServletRequest req, HttpServletResponse res, String parameterName)
+        throws IOException {
+        String id = req.getParameter(parameterName);
+        if ((id == null) || id.isBlank()) {
+            res.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing parameter '" + parameterName + "'");
+            return null;
         }
 
-        String id = req.getParameter("id");
-        if (!DozBibEntryServlet.isValidID(id)) {
-            res.sendError(HttpServletResponse.SC_NOT_FOUND);
-            return;
+        if (!MCRObjectID.isValid(id)) {
+            res.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid object ID '" + id + "'");
+            return null;
         }
 
         MCRObjectID oid = MCRObjectID.getInstance(id);
+        if (!MCRMetadataManager.exists(oid)) {
+            res.sendError(HttpServletResponse.SC_BAD_REQUEST, "No object with ID '" + id + "'");
+            return null;
+        }
+
         new ParentChildRelationChecker(oid, false).check();
 
-        MCRObject obj = MCRMetadataManager.retrieveMCRObject(oid);
-
-        String action = req.getParameter("action");
-        if ("delete".equals(action)) {
-            deleteEntry(obj, res);
-            oid = MCRObjectID.getInstance(req.getParameter("from"));
-        } else {
-            if ("extractHost".equals(action)) {
-                extractHostFrom(obj);
-            } else if ("linkHost".equals(action)) {
-                MCRObjectID parentID = MCRObjectID.getInstance(req.getParameter("host"));
-                new ParentChildRelationChecker(parentID, false).check();
-                linkHost(obj, parentID);
-            } else if ("unlinkHost".equals(action)) {
-                unlinkHostIn(obj);
-                new ParentChildRelationChecker(oid, false).check();
-                oid = MCRObjectID.getInstance(req.getParameter("from"));
-            } else {
-                res.sendError(HttpServletResponse.SC_BAD_REQUEST, "missing 'action' parameter");
-                return;
-            }
-
-            MCRMetadataManager.update(obj);
-            new ParentChildRelationChecker(oid, false).check();
-        }
-
-        res.sendRedirect(MCRServlet.getServletBaseURL() + "DozBibEntryServlet?id=" + oid + "&XSL.Style=structure");
+        return oid;
     }
 
-    private void deleteEntry(MCRObject obj, HttpServletResponse res) throws Exception {
-        LOGGER.info("UBO delete entry " + obj.getId());
+    private void deleteEntry(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        MCRObjectID oid = getAndCheck(req, res, "id");
+        LOGGER.info("UBO delete entry " + oid);
 
-        // do not delete entries that have linked children, otherwise the children would be deleted too
-        List<MCRMetaLinkID> children = obj.getStructure().getChildren();
-        if (!children.isEmpty()) {
-            res.sendError(HttpServletResponse.SC_CONFLICT, "entry has " + children.size() + " child(ren)");
+        MCRObject obj = MCRMetadataManager.retrieveMCRObject(oid);
+        deleteIfNoChildren(res, obj);
+    }
+
+    // do not delete entries that have linked children, otherwise the children would be deleted too
+    private void deleteIfNoChildren(HttpServletResponse res, MCRObject obj)
+        throws IOException, MCRActiveLinkException, MCRAccessException {
+        if (!obj.getStructure().getChildren().isEmpty()) {
+            res.sendError(HttpServletResponse.SC_CONFLICT, "entry has child(ren), wont delete");
+        } else {
+            MCRMetadataManager.deleteMCRObject(obj.getId());
         }
-        MCRMetadataManager.deleteMCRObject(obj.getId());
     }
 
     /** Extract mods:relatedItem[@type='host'] to a new separate entry and link it via @xlink:href */
-    private void extractHostFrom(MCRObject obj)
-        throws Exception {
-        LOGGER.info("UBO extract host in entry " + obj.getId());
+    private void extractHost(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        MCRObjectID oid = getAndCheck(req, res, "id");
+        LOGGER.info("UBO extract host in entry " + oid);
 
+        MCRObject obj = MCRMetadataManager.retrieveMCRObject(oid);
         MCRMODSWrapper wrapper = new MCRMODSWrapper(obj);
 
         // Just add an empty dummy ID as xlink:href attribute, this triggers
         // MCRExtractRelatedItemsEventHandler which will do the actual work afterwards
-        String nullID = MCRObjectID.formatID(obj.getId().getBase(), 0);
+        String nullID = MCRObjectID.formatID(oid.getBase(), 0);
 
         Element hostItem = wrapper.getElement("mods:relatedItem[@type='host']");
         hostItem.setAttribute("href", nullID, MCRConstants.XLINK_NAMESPACE);
+
+        MCRMetadataManager.update(obj);
+        new ParentChildRelationChecker(oid, false).check();
     }
 
     /** Link host in mods:relatedItem[@type='host']/@xlink:href and parent */
-    private void linkHost(MCRObject obj, MCRObjectID parentID)
-        throws Exception {
-        LOGGER.info("UBO link entry " + obj.getId() + " to host " + parentID);
+    private void linkHost(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        MCRObjectID childID = getAndCheck(req, res, "child");
+        MCRObjectID parentID = getAndCheck(req, res, "parent");
 
-        obj.getStructure().setParent(parentID);
+        LOGGER.info("UBO link entry " + childID + " to host " + parentID);
+        setParent(childID, parentID);
 
-        MCRObject parent = MCRMetadataManager.retrieveMCRObject(parentID);
-        MCRMODSWrapper parentWrapper = new MCRMODSWrapper(parent);
-        List<Content> parentContent = parentWrapper.getMODS().removeContent();
+        new ParentChildRelationChecker(childID, false).check();
+    }
 
-        MCRMODSWrapper childWrapper = new MCRMODSWrapper(obj);
+    private void setParent(MCRObjectID childID, MCRObjectID parentID) throws MCRAccessException {
+        LOGGER.info("UBO set parent of " + childID + " to " + parentID);
+
+        MCRObject child = MCRMetadataManager.retrieveMCRObject(childID);
+
+        child.getStructure().setParent(parentID);
+
+        MCRMODSWrapper childWrapper = new MCRMODSWrapper(child);
         Element relatedItem = childWrapper.getElement("mods:relatedItem[@type='host']");
         relatedItem.setAttribute("href", parentID.toString(), MCRConstants.XLINK_NAMESPACE);
-        Element part = relatedItem.getChild("part", MCRConstants.MODS_NAMESPACE).detach();
-        relatedItem.setContent(parentContent);
-        relatedItem.addContent(part);
+
+        MCRMetadataManager.update(child);
     }
 
     /** Unlink host in mods:relatedItem[@type='host']/@xlink:href and parent */
-    private void unlinkHostIn(MCRObject obj)
-        throws Exception {
-        LOGGER.info("UBO unlink host in entry " + obj.getId());
+    private void unlinkHost(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        MCRObjectID childID = getAndCheck(req, res, "child");
+        LOGGER.info("UBO unlink host in entry " + childID);
 
-        obj.getStructure().removeParent();
+        MCRObject child = MCRMetadataManager.retrieveMCRObject(childID);
+        child.getStructure().removeParent();
 
-        MCRMODSWrapper wrapper = new MCRMODSWrapper(obj);
+        MCRMODSWrapper wrapper = new MCRMODSWrapper(child);
         Element host = wrapper.getElement("mods:relatedItem[@type='host']");
         host.getAttribute("href", MCRConstants.XLINK_NAMESPACE).detach();
+
+        MCRMetadataManager.update(child);
+        new ParentChildRelationChecker(childID, false).check();
+    }
+
+    private void merge(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        MCRObjectID intoID = getAndCheck(req, res, "into");
+        MCRObjectID fromID = getAndCheck(req, res, "from");
+        LOGGER.info("UBO merge " + fromID + " into " + intoID);
+
+        MCRObject objInto = MCRMetadataManager.retrieveMCRObject(intoID);
+        MCRObject objFrom = MCRMetadataManager.retrieveMCRObject(fromID);
+
+        Element modsToMergeInto = new MCRMODSWrapper(objInto).getMODS();
+        Element modsToMergeFrom = new MCRMODSWrapper(objFrom).getMODS();
+        MCRMergeTool.merge(modsToMergeInto, modsToMergeFrom);
+
+        MCRMetadataManager.update(objInto);
+
+        List<MCRMetaLinkID> childrenToAdopt = objFrom.getStructure().getChildren();
+        while (!childrenToAdopt.isEmpty()) {
+            MCRMetaLinkID childLink = childrenToAdopt.get(0);
+            childrenToAdopt.remove(childLink);
+
+            MCRObjectID childID = childLink.getXLinkHrefID();
+            setParent(childID, intoID);
+        }
+
+        deleteIfNoChildren(res, objFrom);
+        new ParentChildRelationChecker(intoID, false).check();
     }
 }
