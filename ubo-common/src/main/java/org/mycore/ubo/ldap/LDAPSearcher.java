f5d81167e7e567e18f3d8e94e7b0e0c4d01ce23e
Possommi <Possommi@users.noreply.github.com>
2022-07-27 08:47:25 +0200

    UBO-171 Do not die on ldap response with many entries (#196)
    * UBO-171 Do not die on ldap response with many entries
    * UBO-171 Added paging support for ldap queries

--- ubo-common/src/main/java/org/mycore/ubo/ldap/LDAPSearcher.java
+++ ubo-common/src/main/java/org/mycore/ubo/ldap/LDAPSearcher.java
@@ -1,21 +1,26 @@
 package org.mycore.ubo.ldap;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
+import javax.naming.SizeLimitExceededException;
 import javax.naming.directory.Attribute;
 import javax.naming.directory.Attributes;
-import javax.naming.directory.DirContext;
 import javax.naming.directory.SearchControls;
 import javax.naming.directory.SearchResult;
-
-import org.mycore.common.config.MCRConfiguration2;
+import javax.naming.ldap.Control;
+import javax.naming.ldap.LdapContext;
+import javax.naming.ldap.PagedResultsControl;
+import javax.naming.ldap.PagedResultsResponseControl;
 
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.mycore.common.config.MCRConfiguration2;
 
 /**
  * Given a current (authenticated/active) DirContext, manage searches against the connected LDAP server, returning
@@ -28,37 +33,62 @@ import com.google.common.collect.Multimap;
  */
 public class LDAPSearcher {
 
-    public List<LDAPObject> searchWithGlobalDN(DirContext ctx, String filter) throws NamingException {
+    private static int PAGE_SIZE = MCRConfiguration2.getInt("MCR.user2.LDAP.PageSize").orElse(100);
+
+    private static final Logger LOGGER = LogManager.getLogger(LDAPSearcher.class);
+
+    public List<LDAPObject> searchWithGlobalDN(LdapContext ctx, String filter) throws NamingException {
         String globalDN = MCRConfiguration2.getString(LDAPAuthenticator.CONFIG_PREFIX + "GlobalDN").get();
 
         return search(ctx, globalDN, filter);
     }
 
-    public List<LDAPObject> search(DirContext ctx, String principal, String filter) throws NamingException {
-
+    public List<LDAPObject> search(LdapContext ctx, String principal, String filter) throws NamingException {
         List<LDAPObject> ldapObjects = new ArrayList<>();
-
         SearchControls controls = new SearchControls();
         controls.setSearchScope(SearchControls.SUBTREE_SCOPE);
 
-        NamingEnumeration<SearchResult> results = ctx.search(principal, filter, controls);
+        byte[] cookie = null;
 
-        while (results.hasMore()) {
-            SearchResult searchResult = results.next();
-            Attributes attributes = searchResult.getAttributes();
-            Multimap<String, String> attributeMultiMap = ArrayListMultimap.create();
+        try {
+            ctx.setRequestControls(new Control[] { new PagedResultsControl(PAGE_SIZE, Control.NONCRITICAL) });
 
-            for (NamingEnumeration<String> attributeIDs = attributes.getIDs(); attributeIDs.hasMore();) {
-                String attributeID = attributeIDs.next();
-                Attribute attribute = attributes.get(attributeID);
+            do {
+                NamingEnumeration<SearchResult> results = ctx.search(principal, filter, controls);
+                while (results != null && results.hasMore()) {
+                    SearchResult searchResult = results.next();
+                    Attributes attributes = searchResult.getAttributes();
+                    Multimap<String, String> attributeMultiMap = ArrayListMultimap.create();
 
-                for (NamingEnumeration<?> values = attribute.getAll(); values.hasMore();) {
-                    String attributeValue = values.next().toString();
-                    attributeMultiMap.put(attributeID, attributeValue);
+                    for (NamingEnumeration<String> attributeIDs = attributes.getIDs(); attributeIDs.hasMore(); ) {
+                        String attributeID = attributeIDs.next();
+                        Attribute attribute = attributes.get(attributeID);
+
+                        for (NamingEnumeration<?> values = attribute.getAll(); values.hasMore(); ) {
+                            String attributeValue = values.next().toString();
+                            attributeMultiMap.put(attributeID, attributeValue);
+                        }
+                    }
+
+                    ldapObjects.add(new LDAPObject(searchResult, attributeMultiMap));
                 }
-            }
 
-            ldapObjects.add(new LDAPObject(searchResult, attributeMultiMap));
+                Control[] responseControls = ctx.getResponseControls();
+                if (responseControls != null) {
+                    for (int i = 0; i < responseControls.length; i++) {
+                        if (responseControls[i] instanceof PagedResultsResponseControl) {
+                            PagedResultsResponseControl responseControl = (PagedResultsResponseControl) responseControls[i];
+                            cookie = responseControl.getCookie();
+                        }
+                    }
+                }
+                ctx.setRequestControls(new Control[] { new PagedResultsControl(PAGE_SIZE, cookie, Control.CRITICAL) });
+            } while (cookie != null);
+
+        } catch (SizeLimitExceededException slee) {
+            LOGGER.error("LDAP size limit allegedly exceeded", slee);
+        } catch (IOException ioe) {
+            LOGGER.error("Error setting setting RequestControls to ldap context", ioe);
         }
 
         return ldapObjects;
