42508634c30fdeb4746a6139be4a683d12e53b9d
Silvio Hermann <s.hermann@uni-jena.de>
2023-01-03 13:58:26 +0100

    UBO-208 Perform additional local search to find matching local users

--- ubo-common/src/main/java/org/mycore/ubo/resources/UBOPersonSearchResource.java
+++ ubo-common/src/main/java/org/mycore/ubo/resources/UBOPersonSearchResource.java
@@ -18,21 +18,18 @@
 
 package org.mycore.ubo.resources;
 
-import com.google.gson.Gson;
-import org.mycore.access.MCRAccessManager;
-import org.mycore.common.MCRSession;
-import org.mycore.common.MCRSessionMgr;
-import org.mycore.common.MCRSystemUserInformation;
-import org.mycore.common.MCRUserInformation;
-import org.mycore.common.config.MCRConfiguration2;
-import org.mycore.ubo.picker.IdentityService;
-import org.mycore.ubo.picker.PersonSearchResult;
-
 import javax.naming.OperationNotSupportedException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+
+import com.google.gson.Gson;
+import org.mycore.access.MCRAccessManager;
+import org.mycore.common.config.MCRConfiguration2;
+import org.mycore.ubo.local.LocalService;
+import org.mycore.ubo.picker.IdentityService;
+import org.mycore.ubo.picker.PersonSearchResult;
 
 @Path(UBOPersonSearchResource.PATH)
 public class UBOPersonSearchResource {
@@ -40,23 +37,29 @@ public class UBOPersonSearchResource {
     private static final String STRATEGY_CONFIG_STRING = "MCR.IdentityPicker.strategy";
 
     private static final String STRATEGY_CLASS_SUFFIX = "Service";
+
     public static final String PATH = "search/person";
 
     @GET
     public Response search(@QueryParam("query") String searchQuery) {
         String id = "webpage:/rsc/" + PATH;
-        if(MCRAccessManager.hasRule(id, "read")){
-            if(!MCRAccessManager.checkPermission(id, "read")){
+        if (MCRAccessManager.hasRule(id, "read")) {
+            if (!MCRAccessManager.checkPermission(id, "read")) {
                 return Response.status(Response.Status.FORBIDDEN).build();
             }
         }
 
         String classPrefix = MCRConfiguration2.getStringOrThrow(STRATEGY_CONFIG_STRING);
-        IdentityService service = (IdentityService) MCRConfiguration2.instantiateClass(classPrefix + STRATEGY_CLASS_SUFFIX);
-
+        IdentityService service = MCRConfiguration2.instantiateClass(classPrefix + STRATEGY_CLASS_SUFFIX);
 
         try {
             PersonSearchResult results = service.searchPerson(searchQuery);
+            if (!LocalService.class.getName().equals(classPrefix + STRATEGY_CLASS_SUFFIX)) {
+                LocalService ls = new LocalService();
+                PersonSearchResult localSr = ls.searchPerson(searchQuery);
+                results.personList.addAll(0, localSr.personList);
+            }
+
             return Response.ok(new Gson().toJson(results, PersonSearchResult.class)).build();
         } catch (OperationNotSupportedException e) {
             return Response.status(Response.Status.NOT_IMPLEMENTED).build();
