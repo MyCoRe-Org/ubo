fec86a8d60bc823d1023ee085b0011db305f8c40
Silvio Hermann <s.hermann@uni-jena.de>
2024-06-05 13:29:04 +0200

    UBO-249 Moved functions in ImportList.js to javascript object

--- ubo-common/src/main/resources/META-INF/resources/js/ImportList.js
+++ ubo-common/src/main/resources/META-INF/resources/js/ImportList.js
@@ -2,33 +2,35 @@ $(document).ready(function () {
     $('select[class*="autocomplete"]').chosen({search_contains: true});
 });
 
-function toggleEnrichment(select) {
-    if ("PPNList DOIList".includes($(select).val())) {
-        $("#enrich-yes").click();
-        $("#enrich-no").attr("disabled", "disabled");
-    } else {
-        $("#enrich-no").removeAttr("disabled");
-    }
-}
+const UBOImportList = {
+    toggleEnrichment: function (select) {
+        if ("PPNList DOIList".includes($(select).val())) {
+            $("#enrich-yes").click();
+            $("#enrich-no").attr("disabled", "disabled");
+        } else {
+            $("#enrich-no").removeAttr("disabled");
+        }
+    },
 
-function toggleSelect(elementId, value) {
-    let element = document.getElementById(elementId);
+    toggleSelect: function (elementId, value) {
+        let element = document.getElementById(elementId);
 
-    if (element.getAttribute("disabled") == null) {
-        element.setAttribute("disabled", "disabled");
-        element.value = value;
-    } else {
-        element.removeAttribute("disabled");
-        element.selectedIndex = 0;
-    }
-}
+        if (element.getAttribute("disabled") == null) {
+            element.setAttribute("disabled", "disabled");
+            element.value = value;
+        } else {
+            element.removeAttribute("disabled");
+            element.selectedIndex = 0;
+        }
+    },
 
-function toggleSubmit() {
-    let v = $("textarea").val();
+    toggleSubmit: function () {
+        let v = $("textarea").val();
 
-    if (v === "undefined" || v.length == 0) {
-        $("#submitBtn").attr("disabled", "disabled");
-    } else {
-        $("#submitBtn").removeAttr("disabled");
+        if (v === "undefined" || v.length == 0) {
+            $("#submitBtn").attr("disabled", "disabled");
+        } else {
+            $("#submitBtn").removeAttr("disabled");
+        }
     }
 }
