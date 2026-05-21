9664c3263679ce9a97905c417be195107b33158c
Silvio Hermann <s.hermann@uni-jena.de>
2026-05-21 09:25:49 +0200

    UBO-481 Replaced attribute `data-dismiss` with `data-bs-dismiss`

--- ubo-common/src/main/vue/dedup/src/components/Modal.vue
+++ ubo-common/src/main/vue/dedup/src/components/Modal.vue
@@ -6,7 +6,7 @@
           <h5 class="modal-title">
             <slot name="titel"></slot>
           </h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
